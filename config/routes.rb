7a539a61710bc97c612810c51620b6bbe634b087
Jens Windmueller <jensw.windmueller@gmail.com>
2021-03-03 17:39:58 +0100

    minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/components", to: "pages#components"
-  resources :users, only: [ :show ] do
-    resources :stroll_setting, only: [ :show, :edit, :create, :update ]
-  end
+  resources :users, only: [ :show ]
+  resources :stroll_setting, only: [ :edit, :create, :update ]
+
 
   resources :locations, only: [ :new, :create, :show, :index ]
   resources :walks do

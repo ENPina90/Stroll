569497929cf34da6e689e36751dec83a5643af08
Cynthia Herberg <cynthia_herberg@hotmail.de>
2021-03-03 11:20:53 +0100

    just to merge master

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -1,3 +1,18 @@
-class WalksController < ApplicationController
+require 'json'
+require 'open-uri'
 
+class WalksController < ApplicationController
+  # before_action :set_walk, only: [:show, :edit, :update, :destroy]
+  def show
+    authorize @walk
+    url = "https://api.mapbox.com/directions/v5/mapbox/walking/#{@walk.starting_location.latitude}%2C#{@walk.starting_location.longitude}%3B#{@walk.ending_location.latitude}%2C#{@walk.ending_location.longitude}?alternatives=false&geometries=geojson&steps=true&access_token=#{ENV['MAPBOX_API_KEY']}"
+    route_serialized = open(url).read
+    route = JSON.parse(route_serialized)
+  end
+
+  # private
+
+  # def set_walk
+  #   @walk = Walk.find
+  # end
 end

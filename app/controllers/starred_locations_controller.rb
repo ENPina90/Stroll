59ac606cc79f47ae0a8ab2b1e709bbabc3f2bb06
Cynthia Herberg <cynthia_herberg@hotmail.de>
2021-03-09 14:11:40 +0100

    added starred location index, different markers on map and small detail on starting_loc edit

--- app/controllers/starred_locations_controller.rb
+++ app/controllers/starred_locations_controller.rb
@@ -1,4 +1,19 @@
 class StarredLocationsController < ApplicationController
+  def index
+    @user = current_user
+    @starred_locations = StarredLocation.where(user: current_user)
+    skip_policy_scope
+    authorize @starred_locations
+
+    @markers = @starred_locations.map do |starred_location|
+      {
+        lat: Location.find(starred_location.location_id).latitude,
+        lng: Location.find(starred_location.location_id).longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { starred_location: starred_location }),
+        image_url: helpers.asset_url('star2.jpg')
+      }
+    end
+  end
 
   def new
     @starred_location = StarredLocation.new
@@ -16,12 +31,5 @@ class StarredLocationsController < ApplicationController
       flash[:alert] = "Location not saved"
       redirect_to(locations_path)
     end
-  end
-
-  def index
-    @user = current_user
-    @starred_locations = StarredLocation.where(user: current_user)
-    skip_policy_scope
-    authorize @starred_locations
   end
 end
